b9a71316e8532e40e5b46d754107a0bd60a3bd6a
KooBonSang <koobs97@naver.com>
2023-11-19 22:39:33 +0900

    1. 배치 initialize-schema 옵션 NEVER 로 변경 2. spring security 소스 변경 -> web.ignore() 제거

--- demo/src/main/java/com/example/demo/common/SpringSecurity/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/common/SpringSecurity/SecurityConfig.java
@@ -2,37 +2,42 @@ package com.example.demo.common.SpringSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().antMatchers("*");
-    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests(requests -> requests
-                .mvcMatchers("/", "/findAll", "/login", "/batch*").permitAll()  //해당 패턴의 url만 허용
-                .antMatchers(HttpMethod.GET, "/api*").permitAll()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests ->requests
+                .mvcMatchers("/", "/login", "/batch*", "/findAll").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated());
 
-        http.formLogin(login -> login
+        http.formLogin(login ->login
                 .loginPage("/login")
                 .permitAll());
 
-        http.logout(logout -> logout
+        http.logout(logout ->logout
                 .logoutSuccessUrl("/"));
 
-        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 
-            return http.build();
+    @Bean
+    @Order(0)
+    public SecurityFilterChain resources(HttpSecurity http) throws Exception {
+        return http.requestMatchers(matchers -> matchers.antMatchers( "/resources/**"))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .requestCache(RequestCacheConfigurer::disable)
+                .securityContext(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable).build();
     }
 }

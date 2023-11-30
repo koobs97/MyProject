aef1a9b1ec6fd359a343fae58583f7e315621a57
KooBonSang <koobs97@naver.com>
2023-12-01 00:05:17 +0900

    배치 실행 테스트 코드 구현

---
+++ demo/src/test/java/com/example/demo/Batch001Test.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+
+@SpringBootTest(classes = DemoApplication.class)
+public class Batch001Test {
+
+    private static String JOB_NAME = "TaskletSampleJob";
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobRegistry jobRegistry;
+
+    @DynamicPropertySource
+    static void JobNnameProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.batch.job.name", () -> JOB_NAME );
+    }
+
+    @Test
+    public void Btch001Test() throws Exception {
+        
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("currentTime", System.currentTimeMillis())
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob(JOB_NAME), jobParameters);
+    }
+    
+}
